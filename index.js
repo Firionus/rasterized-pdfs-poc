26f6c25a66f2e3465f9bb461c5ced2f117155b1a
Johannes Fried <jcmf.schule@gmail.com>
2023-10-31 19:10:22 +0100

    Handle all page formats

--- index.js
+++ index.js
@@ -15,24 +15,20 @@ async function onFileSelect(e) {
     let pdfFile = this.files[0];
     let fileName = pdfFile.name;
     console.log("file is", pdfFile);
-    
+
     document.getElementById("progress_indicator").innerHTML = `Processing ${fileName} ...`;
-    
+
     // Convert to images
 
     let pdfData = await pdfFile.arrayBuffer();
 
     // let pdfReader = new FileReader();
     //let pdfData = pdfReader.readAsArrayBuffer(pdfFile);
-    
+
     console.log(pdfData);
     let pdf = await pdfjs.getDocument(pdfData).promise;
 
-    // Default export is a4 paper, portrait, using millimeters for units
-    const doc = new jsPDF({
-        putOnlyUsedFonts: true,
-        compress: true,
-    });
+
 
 
     console.log(pdf);
@@ -40,29 +36,46 @@ async function onFileSelect(e) {
     for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber++) {
         let page = await pdf.getPage(pageNumber);
         var scale = 2;
-        var viewport = page.getViewport({scale: scale});
-    
+        var viewport = page.getViewport({ scale: scale });
+
         // Prepare canvas using PDF page dimensions
         var context = canvas.getContext('2d');
         canvas.height = viewport.height;
         canvas.width = viewport.width;
-    
+
         var renderContext = {
             canvasContext: context,
             viewport: viewport
-          };
+        };
         await page.render(renderContext).promise;
 
-        if (pageNumber != 1) {
-            doc.addPage();
+        const width = viewport.width/scale; // in pt
+        const height = viewport.height/scale; // in pt
+
+        if (width <= height) {
+            var orientation = "portrait"
+        } else {
+            var orientation = "landscape"
+        };
+
+        if (pageNumber == 1) {
+            var doc = new jsPDF({
+                orientation: orientation,
+                unit: "pt",
+                format: [width, height], // order does not matter, is set by orientation
+                putOnlyUsedFonts: true,
+                compress: true,
+            });
+        } else {
+            doc.addPage([width, height], orientation);
         }
         doc.addImage(
             canvas,
             "PNG",
             0,
             0,
-            210,
-            297,
+            width,
+            height,
             `Page ${pageNumber}`,
             "SLOW"
         )
@@ -71,9 +84,9 @@ async function onFileSelect(e) {
     console.log(fileName);
     let nameBeginning = fileName.substring(0, fileName.lastIndexOf("."));
     doc.save(`${nameBeginning} rasterized.pdf`);
-    
 
-   document.getElementById("progress_indicator").innerHTML = "Done. Document was downloaded to your computer.";
+
+    document.getElementById("progress_indicator").innerHTML = "Done. Document was downloaded to your computer.";
 }
 
 document.getElementById("in").addEventListener("change", onFileSelect);
